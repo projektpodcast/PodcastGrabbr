ced004d256f0a269f6d15816accebc92a597c1e8
w0rldpeace <55877541+w0rldpeace@users.noreply.github.com>
2019-10-25 15:12:58 +0200

    Add files via upload

---
+++ GetObjects.cs
@@ -0,0 +1,29 @@
+﻿using CommonTypes;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class GetObjects
+    {
+        public List<Show> GetShowList(int sourceType)
+        {
+            IDataSource fileSource = Factory.CreateFileSource(sourceType);
+            List<Show> allShowList = fileSource.GetAllShows();
+            return allShowList;
+        }
+
+        public List<Show> GetLocalMedia()
+        {
+            MediaDataSource dal = new MediaDataSource();
+
+            dal.GetAllShows();
+            return null;
+            
+        }
+    }
+}
---
+++ SaveObjects.cs
@@ -0,0 +1,30 @@
+﻿using CommonTypes;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class SaveObjects
+    {
+        public void SavePodcastAsXml(Podcast podcast, int targetType)
+        {
+            IDataTarget fileTarget = Factory.CreateFileTarget(targetType);
+            fileTarget.SavePodcast(podcast);
+        }
+
+        public void SavePodcastAsMediaFile(Podcast podcast)
+        {
+            MediaDataTarget mediaTarget = new MediaDataTarget();
+
+            Episode selectedEpisode = podcast.EpisodeList[0];
+            Show selectedShow = podcast.ShowInfo;
+            Podcast selectedPodcastData = new Podcast() { ShowInfo = selectedShow, EpisodeList = new List<Episode> { selectedEpisode } };
+
+            mediaTarget.SavePodcast(selectedPodcastData);
+        }
+    }
+}
