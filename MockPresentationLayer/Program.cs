1ed42d3d2b5bc621cd8b8efb3612eb5287a87313
Philipp Grundmann <philipp.grundmann@hotmail.de>
2019-11-03 18:31:30 +0100

    reset to old version of manfredo

--- MockPresentationLayer/Program.cs
+++ MockPresentationLayer/Program.cs
@@ -27,9 +27,80 @@ namespace MockPresentationLayer
             Podcast a = deserializer.DeserializeRssXml("https://www1.wdr.de/radio/podcasts/wdr2/kabarett132.podcast");
             ////Podcast a = deserializer.DeserializeRssXml("http://www1.swr.de/podcast/xml/swr2/forum.xml");
 
-            PostgresData PostDB = new PostgresData();
+            PostgresData PostDbInstance = new PostgresData();
             //call postdb
-            //PostDB.(a);
+            PostDB(a);
+        }
+
+        static void PostDB(Podcast a)
+        {
+
+            // new Object to connect with Postgres
+            PostgresData myConnection = new PostgresData();
+
+            // dadaBase Name
+            string dbName = "test_podcast1";
+
+            // check if the DB exist
+            Boolean dbExist = myConnection.CheckDatenBank(dbName);
+            if (dbExist == false)
+            {
+                // create db if not exist.
+                bool ifcreated = myConnection.Createdb(dbName);
+                if (ifcreated == false)
+                {
+                    Console.WriteLine("ERROR:");
+                    Console.WriteLine("make sure you have pgAdmin open");
+                    System.Threading.Thread.Sleep(5000);
+                    System.Environment.Exit(1);
+                }
+                else
+                {
+                    myConnection.CreateTable(dbName);
+                    Console.WriteLine("The db cas created");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("The db ist already to use");
+            }
+
+            //read values from Shows
+            string _PublisherName = a.ShowInfo.PublisherName;
+            string _PodcastTitle = a.ShowInfo.PodcastTitle;
+            string Keywords = a.ShowInfo.Keywords;
+            string _Subtitle = a.ShowInfo.Subtitle;
+            string _Language = a.ShowInfo.Language;
+            string _Description = a.ShowInfo.Description;
+            DateTime LastUpdated = a.ShowInfo.LastUpdated;
+            DateTime LastBuildDate = a.ShowInfo.LastBuildDate;
+            string ImageUri = a.ShowInfo.ImageUri;
+            string _Categorie = "new";
+
+            // inster values in shows
+            myConnection.InsertValuesShows(dbName, _PublisherName, _Description, _PublisherName, _Categorie, _Language, ImageUri, LastUpdated, LastBuildDate);
+
+
+
+            // read episodios
+            foreach (var Episode in a.EpisodeList)
+            {
+                string _Title = Episode.Title;
+                _Title = _Title.Replace("'", "''");
+                DateTime _PublishDate = Episode.PublishDate;
+                string _Summary = Episode.Summary;
+                _Summary = _Summary.Replace("'", "''");
+                string _Keywords = Episode.Keywords;
+                string _ImageUri = Episode.ImageUri;
+                string _FileDetails = Episode.FileDetails.SourceUri;
+
+                // inser values in each episode
+                myConnection.InsertValuesDownload(dbName, _FileDetails);
+                myConnection.InsertValuesEpisodes(dbName, _Title, _PublishDate, _Summary, _Keywords, _ImageUri, _FileDetails);
+            }
+
+
         }
     }
 }
